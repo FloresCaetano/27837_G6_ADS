5326c1ff78270cc00e2c434adce8e4cfe05aee6c
JordanESPE <jaguaman16@espe.edu.ec>
2025-11-27 06:51:12 -0500

    CRUD 3 TALLER 4

---
+++ U2/U2T4/CRUD3/src/main/java/ec/edu/espe/observer/EstudianteSubject.java
@@ -0,0 +1,7 @@
+package ec.edu.espe.observer;
+
+public interface EstudianteSubject {
+	void agregarObservador(EstudianteObserver observer);
+	void removerObservador(EstudianteObserver observer);
+	void notificarObservadores();
+}
